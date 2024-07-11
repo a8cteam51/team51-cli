00e5cc4b77c05788ef105208dc7fe564d84a14cf
ninomihovilic <nino.mihovilic@gmail.com>
2024-07-11 18:16:39 +0800

    Add WPCOM create staging command

--- includes/connection-helper-wpcom.php
+++ includes/connection-helper-wpcom.php
@@ -14,19 +14,48 @@ final class WPCOM_Connection_Helper {
 	 */
 	public const SSH_HOST = 'ssh.atomicsites.net';
 
+	/**
+	 * The SFTP URL.
+	 */
+	public const SFTP_HOST = 'sftp.wp.com';
+
 	// endregion
 
 	// region METHODS
 
 	/**
-	 * Opens a new SSH connection to a WordPress.com site.
+	 * Opens a new SFTP connection to a Pressable site.
 	 *
-	 * @param   string $site_id_or_url The ID or URL of the WordPress.com site to open a connection to.
+	 * @param   string  $site_id_or_url The ID or URL of the Pressable site to open a connection to.
+	 * @param boolean $staging        If the WordPress.com site to open a connection to is a staging site.
 	 *
 	 * @return  SFTP|null
 	 */
-	public static function get_ssh_connection( string $site_id_or_url ): ?SSH2 {
-		$credentials = self::get_credentials( $site_id_or_url );
+	public static function get_sftp_connection( string $site_id_or_url, bool $staging = false ): ?SFTP {
+		$credentials = self::get_credentials( $site_id_or_url, $staging );
+		if ( \is_null( $credentials ) ) {
+			return null;
+		}
+
+		$connection = new SFTP( self::SFTP_HOST );
+		if ( ! $connection->login( $credentials->username, $credentials->password ) ) {
+			$connection->isConnected() && $connection->disconnect();
+			return null;
+		}
+
+		return $connection;
+	}
+
+	/**
+	 * Opens a new SSH connection to a WordPress.com site.
+	 *
+	 * @param   string  $site_id_or_url The ID or URL of the WordPress.com site to open a connection to.
+	 * @param   boolean $staging        If the WordPress.com site to open a connection to is a staging site.
+	 *
+	 * @return  SFTP|null
+	 */
+	public static function get_ssh_connection( string $site_id_or_url, bool $staging = false ): ?SSH2 {
+		$credentials = self::get_credentials( $site_id_or_url, $staging );
 		if ( \is_null( $credentials ) ) {
 			return null;
 		}
@@ -56,21 +85,31 @@ final class WPCOM_Connection_Helper {
 	/**
 	 * Returns the SFTP/SSH login data for the concierge user on a given WordPress.com site.
 	 *
-	 * @param   string $site_id_or_url The ID or URL of the WordPress.com site to get the login data for.
+	 * @param   string  $site_id_or_url The ID or URL of the WordPress.com site to get the login data for.
+	 * @param boolean $staging        If the WordPress.com site to open a connection to is a staging site.
 	 *
 	 * @return  stdClass|null
 	 */
-	private static function get_credentials( string $site_id_or_url ): ?stdClass {
+	private static function get_credentials( string $site_id_or_url, bool $staging = false ): ?stdClass {
 		static $cache = array();
 
 		if ( empty( $cache[ $site_id_or_url ] ) ) {
-			$ssh_username = get_wpcom_site_ssh_username( $site_id_or_url );
+			if ( $staging ) {
+				$ssh_username = get_wpcom_staging_site_ssh_username( $site_id_or_url );
+			} else {
+				$ssh_username = get_wpcom_site_ssh_username( $site_id_or_url );
+			}
+
 			if ( \is_null( $ssh_username ) ) {
 				console_writeln( '❌ Could not find the WordPress.com site SSH username.' );
 				return null;
 			}
 
-			$cache[ $site_id_or_url ] = rotate_wpcom_site_sftp_user_password( $site_id_or_url, $ssh_username );
+			if ( $staging ) {
+				$cache[ $site_id_or_url ] = rotate_wpcom_staging_site_sftp_user_password( $site_id_or_url, $ssh_username );
+			} else {
+				$cache[ $site_id_or_url ] = rotate_wpcom_site_sftp_user_password( $site_id_or_url, $ssh_username );
+			}
 		}
 
 		return $cache[ $site_id_or_url ];
